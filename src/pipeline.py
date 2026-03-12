cef898440376db2bed35fb1a7f9362f0956d1e86
Aufii Fathin Nabila <aufiifathin@gmail.com>
2026-03-12 16:45:03 +0000

    feat(data): add data pipeline including ingestion and preprocessing for gold, oil, and macro data

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,21 +1,19 @@
-from data.ingestion import main as ingestion
-from data.preprocess import main as preprocess
+from data.data_pipeline import run_data_pipeline
 from models.train import main as train
 from models.predict import main as predict
 
+
 def run_pipeline():
 
-    print("Running ingestion...")
-    ingestion()
-
-    print("Running preprocessing...")
-    preprocess()
+    print("Running data pipeline...")
+    run_data_pipeline()
 
     print("Training model...")
     train()
 
     print("Running prediction...")
     predict()
+
 
 if __name__ == "__main__":
     run_pipeline()
